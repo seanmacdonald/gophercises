a23e85b811868a8202f279d358c3edb53e31b5c7
Sean MacDonald <seandoylemacdonald@gmail.com>
2019-09-13 23:31:33 -0700

    completed parse yaml function

--- ex2/handler.go
+++ ex2/handler.go
@@ -3,6 +3,8 @@ package urlshort
 import (
 	"fmt"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -47,6 +49,44 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	urlInfoMap := parseYaml(yml)
+	fmt.Println(urlInfoMap)
+	
+	//setup handler 
+	var hf http.HandlerFunc
+	hf = func (w http.ResponseWriter, r *http.Request) {
+		fallback.ServeHTTP(w, r)
+	}
+
+	return hf, nil
+}
+
+type UrlInfo struct {
+	Path string `yaml:"path"`
+	Url string	`yaml:"url"`
+}
+
+type UrlInfoMap struct {
+	Records []UrlInfo
+}
+
+// private function for parsing yaml 
+// Here is a good resource for learning how to parse yaml with the library: 
+// http://squarism.com/2014/10/13/yaml-go/ 
+func parseYaml(yml []byte) map[string]string {
+	var b UrlInfoMap
+
+	err := yaml.Unmarshal(yml, &b)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//go through object and creatre map[string]string 
+	urlMap := make(map[string]string) 
+
+	for _, val := range b.Records {
+		urlMap[val.Path] = val.Url
+	}
+
+	return urlMap
 }
