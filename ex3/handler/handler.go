40ebd2bf827f0adb83c0ccf5a5a5214bb5c6c4ad
Sean MacDonald <seandoylemacdonald@gmail.com>
2019-09-14 20:18:25 -0700

    rendered basic html with all required info for all story routes but need to fix hyperlinks still

--- ex3/handler/handler.go
+++ ex3/handler/handler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"fmt"
+	"os"
 	"net/http"
+	"html/template"
 	"github.com/seanmacdonald/gophercises/ex3/story"
 )
 
@@ -14,12 +16,23 @@ func MakeHandler(storyMap map[string]story.Info) (http.HandlerFunc, error) {
 	hf = func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path[1:] //remove first char because its a slash 
 		if info, ok := storyMap[path]; ok {
-			//TODO: call html func to make it look nice 
-			fmt.Fprintln(w, info.Title)
+			renderChapter(info, w)
 		} else {
 			fmt.Fprintln(w, "nothing for: " + path)
 		}
 	}
 
 	return hf, nil
+}
+
+func renderChapter(info story.Info, w http.ResponseWriter) {
+	os.Chdir("../handler")
+	defer os.Chdir("../main")
+
+	tmpl, err := template.ParseFiles("layout.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tmpl.Execute(w, info)
 }
